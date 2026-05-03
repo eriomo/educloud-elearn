64e8a9e85f1335b7f46279bb45fe6a9da2b7c351
eriomo <trigxyfn@gmail.com>
2026-05-03 23:36:22 +0100

    Create 0003_merge.py

---
+++ assessments/migrations/0003_merge.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('assessments', '0002_add_ml_fields'),
+        ('assessments', '0002_ai_feedback'),
+    ]
+
+    operations = [
+    ]
