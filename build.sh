438613b9eae9376a55b5815c57c5b6097f81f3cb
eriomo <trigxyfn@gmail.com>
2026-05-03 23:48:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
 python manage.py create_default_users
-python manage.py train_difficulty_model || echo "Model training skipped - will retry on next deploy"
 python manage.py load_sample_questions || echo "Questions already loaded"
+python manage.py train_difficulty_model || echo "Model training skipped"
